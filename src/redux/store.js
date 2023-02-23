7f63afe36d338c499f9f7e049da7b2c5226c04f6
DimaMarkow <dimamarkow@gmail.com>
2023-02-23 15:08:24 +0200

    slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,16 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import { tasksReducer, filtersReducer } from './reducer';
+
+// export const store = configureStore({
+//   reducer: {
+//     tasks: tasksReducer,
+//     filters: filtersReducer,
+//   },
+// });
+
 import { configureStore } from '@reduxjs/toolkit';
-import { tasksReducer, filtersReducer } from './reducer';
+import { tasksReducer } from './tasksSlice';
+import { filtersReducer } from './filtersSlice';
 
 export const store = configureStore({
   reducer: {
